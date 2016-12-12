496369f25fec7e8df58eba06ffb4a2d6011bdd22
Jarryd460 <Jarryd.deane@gmail.com>
2016-12-12 13:35:46 +0200

    Password Hashed and Salted

---
+++ Payroll Management System/Model/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Payroll_Management_System.Model
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public char Type { get; set; }
+    }
+}
